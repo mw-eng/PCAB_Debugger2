c85da0e9010333b45bdc538a2dea08944da6ec30
omc10079 <kida-seiji@orient-microwave.co.jp>
2025-03-06 08:20:30 +0900

    v1.0.1

--- PCAB_Debugger2_GUI/cntUnit.xaml.cs
+++ PCAB_Debugger2_GUI/cntUnit.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using static PCAB_Debugger2_GUI.Ports;
 
 namespace PCAB_Debugger2_GUI
@@ -104,7 +93,7 @@ namespace PCAB_Debugger2_GUI
                     case "CheckBox_STBLNA": cat = CheckBoxCategory.StandbyLNA; break;
                     case "CheckBox_SETLPM": cat = CheckBoxCategory.LowPowerMode; break;
                 }
-                CheckboxClickEvent?.Invoke(this, e, cat, null);
+                CheckboxClickEvent?.Invoke(this, e, cat, isChecked);
             }
             else
             {
